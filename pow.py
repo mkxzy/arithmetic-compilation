d8d8531dcc40d12ed34473f84a45729559d21630
mike <makina67@sina.com>
2017-12-07 23:08:39 +0800

    指数

---
+++ pow.py
@@ -0,0 +1,20 @@
+import math
+
+a = 5
+
+# a的平方
+pow2 = a ** 2
+print(pow2)
+
+# 另外一种写法
+pow2_2 = math.pow(a, 2)
+print(pow2_2)
+
+# a的立方
+pow3 = math.pow(a, 3)
+print(pow3)
+
+# a的n次方
+n = 6
+pown = math.pow(a, n)
+print(pown)
